99a9fe8e4fc20af4899e3fa96cbbf9a5135a1772
Martin Verzilli <mverzilli@manas.com.ar>
2013-07-24 13:24:39 -0300

    Basic login layout

--- src/org/unicef/gis/LoginActivity.java
+++ src/org/unicef/gis/LoginActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
 	}
 
 	@Override
