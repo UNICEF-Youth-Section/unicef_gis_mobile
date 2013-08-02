252de1e2283f2e53c1c9ca8059077deb34834215
Martin Verzilli <mverzilli@manas.com.ar>
2013-08-02 16:44:10 -0300

    Better placement of controls in FetchTagsActivity

--- src/org/unicef/gis/FetchTagsActivity.java
+++ src/org/unicef/gis/FetchTagsActivity.java
@@ -3,15 +3,24 @@ package org.unicef.gis;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class FetchTagsActivity extends Activity {
 	ProgressBar progress;
+	TextView feedback;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fetch_tags);
 		
+		loadControls();
+		
+		feedback.setText(R.string.wait_configuring);
+	}
+
+	private void loadControls() {
 		progress = (ProgressBar) findViewById(R.id.fetch_tags_activity_progress);
+		feedback = (TextView) findViewById(R.id.fetch_tags_feedback);
 	}
 }
