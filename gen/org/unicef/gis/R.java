bc29852d71b75184255b976bae6b03bc3b368f79
Martin Verzilli <mverzilli@manas.com.ar>
2013-08-02 14:56:15 -0300

    Configure server URL activity

--- gen/org/unicef/gis/R.java
+++ gen/org/unicef/gis/R.java
@@ -24,11 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int edit_email=0x7f090001;
-        public static final int edit_password=0x7f090002;
-        public static final int message=0x7f090000;
-        public static final int my_reports_status=0x7f090003;
+        public static final int action_settings=0x7f090007;
+        public static final int configure_server_url_button_save=0x7f090002;
+        public static final int configure_server_url_edit_url=0x7f090001;
+        public static final int configure_server_url_welcome=0x7f090000;
+        public static final int edit_email=0x7f090004;
+        public static final int edit_password=0x7f090005;
+        public static final int message=0x7f090003;
+        public static final int my_reports_status=0x7f090006;
     }
     public static final class layout {
         public static final int activity_configure_server_url=0x7f030000;
@@ -50,11 +53,14 @@ public final class R {
         public static final int edit_password=0x7f060002;
         public static final int enter_email=0x7f060008;
         public static final int enter_password=0x7f060009;
+        public static final int example_url=0x7f06000f;
         public static final int hello_world=0x7f060007;
         public static final int please_enter_valid_email_and_password=0x7f06000b;
+        public static final int save_address=0x7f060010;
         public static final int title_activity_configure_server_url=0x7f06000d;
         public static final int title_activity_display_message=0x7f060006;
         public static final int title_activity_login=0x7f060005;
+        public static final int welcome=0x7f06000e;
     }
     public static final class style {
         /** 
